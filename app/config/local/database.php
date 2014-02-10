462c5f3b19cb86c846be096e4281d32c4668dc0f
Thomas Warlop <thomas.warlop@gmail.com>
2014-02-10 22:09:21 +0100

    setup database

---
+++ app/config/local/database.php
@@ -0,0 +1,38 @@
+<?php
+
+return array(
+
+    'default' => 'mysql',
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'port'      => '8889',
+            'database'  => 'eid',
+            'username'  => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+
+    ),
+
+
+    /*
+        |--------------------------------------------------------------------------
+        | Backup settings
+        |--------------------------------------------------------------------------
+        |
+        */
+    'backup' => array(
+        // add the path to the restore and backup command of mysql
+        // this exemple is if your are using MAMP server on a mac
+        'mysql' => array(
+            'dump_command_path' => '/Applications/MAMP/bin/apache2/bin/',
+            'restore_command_path' => '/Applications/MAMP/bin/apache2/bin/',
+        ),
+    ),
+);
