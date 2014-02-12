c7fb3a5f937b910d0ccd9605fd4a2cba49c543cc
Thomas Warlop <thomas.warlop@gmail.com>
2014-02-12 16:54:54 +0100

    implement close enough method in application model

--- app/models/Application/Application.php
+++ app/models/Application/Application.php
@@ -26,4 +26,65 @@ class Application extends \Eloquent{
         return $this->belongsTo('Application\Category\Subcategory', 'subcategory_id');
     }
 
+    /**
+     * Check if the application is within a specified distance
+     * @param $latitude
+     * @param $longitude
+     * @param int $distance
+     */
+    public function closeEnough($latitude, $longitude, $distance = 10)
+    {
+        $difference = $this->distance($latitude, $longitude, $this->Latitude, $this->Longitude, 'K');
+
+        return $difference <= $distance;
+    }
+
+
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::                                                                         :*/
+    /*::  This routine calculates the distance between two points (given the     :*/
+    /*::  latitude/longitude of those points). It is being used to calculate     :*/
+    /*::  the distance between two locations using GeoDataSource(TM) Products    :*/
+    /*::                     													 :*/
+    /*::  Definitions:                                                           :*/
+    /*::    South latitudes are negative, east longitudes are positive           :*/
+    /*::                                                                         :*/
+    /*::  Passed to function:                                                    :*/
+    /*::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :*/
+    /*::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :*/
+    /*::    unit = the unit you desire for results                               :*/
+    /*::           where: 'M' is statute miles                                   :*/
+    /*::                  'K' is kilometers (default)                            :*/
+    /*::                  'N' is nautical miles                                  :*/
+    /*::  Worldwide cities and other features databases with latitude longitude  :*/
+    /*::  are available at http://www.geodatasource.com                          :*/
+    /*::                                                                         :*/
+    /*::  For enquiries, please contact sales@geodatasource.com                  :*/
+    /*::                                                                         :*/
+    /*::  Official Web site: http://www.geodatasource.com                        :*/
+    /*::                                                                         :*/
+    /*::         GeoDataSource.com (C) All Rights Reserved 2014		   		     :*/
+    /*::                                                                         :*/
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    protected function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+
+        $theta = $lon1 - $lon2;
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $miles = $dist * 60 * 1.1515;
+        $unit = strtoupper($unit);
+
+        if ($unit == "K") {
+            return ($miles * 1.609344);
+        } else if ($unit == "N") {
+            return ($miles * 0.8684);
+        } else {
+            return $miles;
+        }
+    }
+
+
+
+
 } 
