8477d13b762529a6effdd53b4bb78ac928413b5d
Thomas Warlop <thomas.warlop@gmail.com>
2014-02-19 23:24:18 +0100

    another fix

--- app/lang/fr/home.php
+++ app/lang/fr/home.php
@@ -6,5 +6,5 @@ return array(
     "search_postal_city" => "Chercher par code postal ou ville",
     "applicaties" => "Applications",
     "thanks_for_signing_up" => "Merci pour enregistrer l'application dans le système",
-    "want_to_join" => "Montrez votre application ici?",
+    "want_to_join" => "Votre application doit également apparaître?",
 );
